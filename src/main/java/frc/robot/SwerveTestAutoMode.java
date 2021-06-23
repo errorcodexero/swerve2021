0123936406dab15910e29d41216448f5ff18685c
Jack (Butch) Griffin <butchg@comcast.net>
2021-06-23 12:49:02 -0700

    added additoinal test modes

--- src/main/java/frc/robot/SwerveTestAutoMode.java
+++ src/main/java/frc/robot/SwerveTestAutoMode.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import org.xero1425.base.actions.InvalidActionRequest;
+import org.xero1425.base.actions.ParallelAction;
 import org.xero1425.base.controllers.TestAutoMode;
 import org.xero1425.base.swervedrive.SwerveDriveSubsystem;
 import org.xero1425.base.swervedrive.SwerveSetMotorPowerAction;
@@ -8,11 +9,13 @@ import org.xero1425.misc.BadParameterTypeException;
 import org.xero1425.misc.MissingParameterException;
 
 public class SwerveTestAutoMode extends TestAutoMode {
-    public SwerveTestAutoMode(SwerveDriveRobotAutoController ctrl) throws BadParameterTypeException, MissingParameterException, InvalidActionRequest {
-        super(ctrl, "Swerver-Test-Mode") ;
+    public SwerveTestAutoMode(SwerveDriveRobotAutoController ctrl)
+            throws BadParameterTypeException, MissingParameterException, InvalidActionRequest {
+        super(ctrl, "Swerver-Test-Mode");
 
-        SwerveDriveRobotSubsystem robotsys = (SwerveDriveRobotSubsystem)ctrl.getRobot().getRobotSubsystem();
-        SwerveDriveSubsystem swerve = (SwerveDriveSubsystem)robotsys.getDB() ;
+        SwerveDriveRobotSubsystem robotsys = (SwerveDriveRobotSubsystem) ctrl.getRobot().getRobotSubsystem();
+        SwerveDriveSubsystem swerve = (SwerveDriveSubsystem) robotsys.getDB();
+        ParallelAction pact = new ParallelAction(ctrl.getRobot().getMessageLogger(), ParallelAction.DonePolicy.All) ;
 
         switch(getTestNumber()) {
             case 0:
@@ -22,23 +25,30 @@ public class SwerveTestAutoMode extends TestAutoMode {
                 addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, 0.0, getPower()), true) ;
                 break ;
             case 2:
-                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, getPower(), 0.0), true) ;
+                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FR, getPower(), 0.0), true) ;
                 break ;
             case 3:
-                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, 0.0, getPower()), true) ;
+                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FR, 0.0, getPower()), true) ;
                 break ;
             case 4:
-                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, getPower(), 0.0), true) ;
+                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.BL, getPower(), 0.0), true) ;
                 break ;
             case 5:
-                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, 0.0, getPower()), true) ;
+                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.BL, 0.0, getPower()), true) ;
                 break ;
             case 6:
-                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, getPower(), 0.0), true) ;
+                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.BR, getPower(), 0.0), true) ;
                 break ;
             case 7:
-                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, 0.0, getPower()), true) ;
-                break ;                                                                                                                
+                addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.BR, 0.0, getPower()), true) ;
+                break ;
+            case 8:
+                pact.addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FL, getPower(), getPower()), true) ;
+                pact.addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.FR, getPower(), getPower()), true) ;
+                pact.addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.BL, getPower(), getPower()), true) ;
+                pact.addSubActionPair(swerve, new SwerveSetMotorPowerAction(swerve, SwerveDriveSubsystem.BR, getPower(), getPower()), true) ;
+                addAction(pact) ;
+                break ;
         }
     }
 }
