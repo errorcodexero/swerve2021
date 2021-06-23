1e9268de6123c7ceb2687dfaab635afa778bab28
Jack (Butch) Griffin <butchg@comcast.net>
2021-06-23 10:23:51 -0700

    updated framework for swerve gamepad, added swerve robot subsystem

---
+++ src/main/java/frc/robot/SwerveDriveRobotSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+import org.xero1425.base.DriveBaseSubsystem;
+import org.xero1425.base.RobotSubsystem;
+import org.xero1425.base.XeroRobot;
+import org.xero1425.base.oi.OISubsystem;
+import org.xero1425.base.swervedrive.SwerveDriveSubsystem;
+
+public class SwerveDriveRobotSubsystem extends RobotSubsystem {
+    public SwerveDriveRobotSubsystem(XeroRobot robot) throws Exception {
+        super(robot, "SwerveRobotSubsystem") ;
+
+        DriveBaseSubsystem db = new SwerveDriveSubsystem(this, "ServerDriveSubsystem", "hw:swervedrive") ;
+        addChild(db) ;
+        addChild(new OISubsystem(this, "swerveoi", db)) ;
+    }
+}
