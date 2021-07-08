0d9c65e0c244951773a51641a898bf1b1b7d5c82
Jack (Butch) Griffin <butchg@comcast.net>
2021-07-08 10:46:54 -0700

    fixed bugs in teleop driving

--- src/main/java/frc/robot/Swerve2021.java
+++ src/main/java/frc/robot/Swerve2021.java
@@ -30,7 +30,7 @@ public class Swerve2021 extends XeroRobot {
         if (ret != null)
             return ret;
 
-        return "testmode";
+        return "teleop";
     }
 
     public AutoController createAutoController() {
