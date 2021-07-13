beaeb47c9e0e773e4310827cf85b882835025750
Jack (Butch) Griffin <butchg@comcast.net>
2021-07-13 13:23:08 -0700

    added new test cases

--- src/main/java/frc/robot/Swerve2021.java
+++ src/main/java/frc/robot/Swerve2021.java
@@ -30,7 +30,7 @@ public class Swerve2021 extends XeroRobot {
         if (ret != null)
             return ret;
 
-        return "fl45mirror";
+        return "clockwise_counter";
     }
 
     public AutoController createAutoController() {
