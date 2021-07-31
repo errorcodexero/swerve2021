919e407eeb4e72c7c6a39846475d60e655c5ab81
Jack (Butch) Griffin <butchg@comcast.net>
2021-07-31 13:21:34 -0700

    updated the swerve robot OI code to match changes to the framework

--- src/main/java/frc/robot/SwerveDriveRobotSubsystem.java
+++ src/main/java/frc/robot/SwerveDriveRobotSubsystem.java
@@ -4,6 +4,8 @@ import org.xero1425.base.RobotSubsystem;
 import org.xero1425.base.XeroRobot;
 import org.xero1425.base.swervedrive.SwerveDriveSubsystem;
 
+import frc.robot.oi.Swerve2021OISubsystem;
+
 public class SwerveDriveRobotSubsystem extends RobotSubsystem {
     public SwerveDriveRobotSubsystem(XeroRobot robot) throws Exception {
         super(robot, "SwerveRobotSubsystem") ;
@@ -11,7 +13,7 @@ public class SwerveDriveRobotSubsystem extends RobotSubsystem {
         SwerveDriveSubsystem db = new SwerveDriveSubsystem(this, "swervedrive", "hw:swervedrive") ;
         addChild(db) ;
 
-        SwerveDriveOI oi = new SwerveDriveOI(this, db) ;
+        Swerve2021OISubsystem oi = new Swerve2021OISubsystem(this, db) ;
         addChild(oi) ;
     }
 }
